64d54d31c48c4f0ff4d92cbaf351c27dab2072e2
Bruno Haible <bruno@clisp.org>
2001-06-12 12:53:21 +0000

    Typo.

--- src/po.h
+++ src/po.h
@@ -35,7 +35,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
    Use a NULL pointer where no action is required.  */
 
-/* Forward decaration.  */
+/* Forward declaration.  */
 struct po_ty;
 
 
