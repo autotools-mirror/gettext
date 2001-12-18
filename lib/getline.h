1d1aa5294dd207fd7ac8e8adabfd8f74a1428d47
Bruno Haible <bruno@clisp.org>
2001-12-18 13:37:32 +0000

    Portability fix.

--- lib/getline.h
+++ lib/getline.h
@@ -17,6 +17,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #ifndef GETLINE_H_
 # define GETLINE_H_ 1
 
+# include <stddef.h>
 # include <stdio.h>
 
 # ifndef PARAMS
