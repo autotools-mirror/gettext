cac7971dcf9e1329e212465c054fd01c12f50030
Bruno Haible <bruno@clisp.org>
2002-01-24 20:19:59 +0000

    Don't disable assert().

--- lib/getline.c
+++ lib/getline.c
@@ -1,6 +1,6 @@
 /* getline.c -- Replacement for GNU C library function getline
 
-Copyright (C) 1993, 1996, 2001 Free Software Foundation, Inc.
+Copyright (C) 1993, 1996, 2001, 2002 Free Software Foundation, Inc.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
@@ -46,7 +46,6 @@ getline (lineptr, n, stream)
 
 #else /* ! have getdelim */
 
-# define NDEBUG
 # include <assert.h>
 
 # if HAVE_STDLIB_H
