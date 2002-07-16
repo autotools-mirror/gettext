d369eb5c88ea146da56c9048f89995953ef2f7d6
Bruno Haible <bruno@clisp.org>
2002-07-16 17:34:49 +0000

    Bump version.

--- misc/po-mode.el
+++ misc/po-mode.el
@@ -50,7 +50,7 @@
 
 ;;; Code:
 
-(defconst po-mode-version-string "2.00" "\
+(defconst po-mode-version-string "2.01" "\
 Version number of this version of po-mode.el.")
 
 ;;; Emacs portability matters - part I.
