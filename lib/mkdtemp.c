9c99d129b70eb7378c87526f6a453f51cf0bd133
Bruno Haible <bruno@clisp.org>
2003-01-14 13:38:43 +0000

    Unify uintmax_t handling with gnulib.

--- lib/mkdtemp.c
+++ lib/mkdtemp.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1999, 2001-2002 Free Software Foundation, Inc.
+/* Copyright (C) 1999, 2001-2003 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -39,11 +39,10 @@
 # define TMP_MAX 238328
 #endif
 
-#if HAVE_STDINT_H_WITH_UINTMAX || _LIBC
+#if HAVE_STDINT_H || _LIBC
 # include <stdint.h>
 #endif
-
-#if HAVE_INTTYPES_H_WITH_UINTMAX || _LIBC
+#if HAVE_INTTYPES_H
 # include <inttypes.h>
 #endif
 
