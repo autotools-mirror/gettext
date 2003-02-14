6ec63daeb67e3d6f14ca2a1f08c4c1b874bf2361
Bruno Haible <bruno@clisp.org>
2003-02-14 21:44:11 +0000

    Adapt to new directory structure.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,27 @@
-# You need autoconf-2.57 and automake-1.7.2.
-aclocal -I m4
+# This script requires autoconf-2.57 and automake-1.7.2 in the PATH.
+
+aclocal
 autoconf
-autoheader
 automake
+
+(cd autoconf-lib-link
+ aclocal -I m4
+ autoconf
+ automake
+)
+
+(cd gettext-runtime
+ aclocal -I m4 -I ../gettext-tools/m4 -I ../autoconf-lib-link/m4 -I ../config/m4
+ autoconf
+ autoheader
+ automake
+)
+
+(cd gettext-tools
+ aclocal -I m4 -I ../gettext-runtime/m4 -I ../autoconf-lib-link/m4 -I ../config/m4
+ autoconf
+ autoheader
+ automake
+)
+
+cp -p autoconf-lib-link/config.rpath config/config.rpath
