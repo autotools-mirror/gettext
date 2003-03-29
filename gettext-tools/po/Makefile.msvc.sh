55330d5547a8ec7848309fc14c6d2ddaf5f78dca
Bruno Haible <bruno@clisp.org>
2003-03-29 16:20:30 +0000

    Native Woe32/MSVC support.

---
+++ gettext-tools/po/Makefile.msvc.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+# Creates Makefile.msvc.
+domain=$1
+catalogs=$2
+
+cat <<\EOF
+# -*- Makefile -*- for po subdirectory
+
+#### Start of system configuration section. ####
+
+# Directories used by "make":
+srcdir = .
+
+# Directories used by "make install":
+prefix = c:\usr
+datadir = $(prefix)\share
+localedir = $(datadir)\locale
+
+# Programs used by "make":
+RM = -del
+
+# Programs used by "make install":
+INSTALL = copy
+INSTALL_PROGRAM = copy
+INSTALL_DATA = copy
+
+#### End of system configuration section. ####
+
+SHELL = /bin/sh
+
+all :
+
+install : all force
+	-mkdir $(prefix)
+	-mkdir $(datadir)
+	-mkdir $(localedir)
+EOF
+for cat in $catalogs; do
+  cat=`basename $cat`
+  lang=`echo $cat | sed -e 's/\.gmo$//'`
+cat <<EOF
+	-mkdir \$(localedir)\\${cat}
+	-mkdir \$(localedir)\\${cat}\\LC_MESSAGES
+	\$(INSTALL_DATA) ${cat}.gmo \$(localedir)\\${cat}\\LC_MESSAGES\\${domain}.mo
+EOF
+done
+cat <<\EOF
+
+installdirs : force
+	-mkdir $(prefix)
+	-mkdir $(datadir)
+	-mkdir $(localedir)
+EOF
+for cat in $catalogs; do
+  cat=`basename $cat`
+  lang=`echo $cat | sed -e 's/\.gmo$//'`
+cat <<EOF
+	-mkdir \$(localedir)\\${cat}
+	-mkdir \$(localedir)\\${cat}\\LC_MESSAGES
+EOF
+done
+cat <<\EOF
+
+uninstall : force
+EOF
+for cat in $catalogs; do
+  cat=`basename $cat`
+  lang=`echo $cat | sed -e 's/\.gmo$//'`
+cat <<EOF
+	\$(RM) \$(localedir)\\${cat}\\LC_MESSAGES\\${domain}.mo
+EOF
+done
+cat <<\EOF
+
+check : all
+
+mostlyclean : clean
+
+clean : force
+	$(RM) core
+
+distclean : clean
+
+maintainer-clean : distclean
+
+force :
+EOF
