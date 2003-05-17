87c6f0b63b0ab5f407850990c33d8a6a15601b8c
Bruno Haible <bruno@clisp.org>
2003-05-17 15:03:33 +0000

    Prepare for release 0.12.

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=0.12-pre4
-RELEASE_DATE=2003-05-06      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.12
+RELEASE_DATE=2003-05-17      # in "date +%Y-%m-%d" format
