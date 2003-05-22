ed9a89a635e884bc81a3ecc9e35b1ae9c1028e7f
Bruno Haible <bruno@clisp.org>
2003-05-22 21:48:13 +0000

    Release 0.12.1.

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=0.12
-RELEASE_DATE=2003-05-17      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.12.1
+RELEASE_DATE=2003-05-22      # in "date +%Y-%m-%d" format
