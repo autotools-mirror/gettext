22763b7499d74bd9f6f0f812d9a957aba16840c3
Bruno Haible <bruno@clisp.org>
2003-11-16 15:29:36 +0000

    Bump version number to 0.13-pre1.

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=0.12.2
-RELEASE_DATE=2003-08-23      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.13-pre1
+RELEASE_DATE=2003-11-16      # in "date +%Y-%m-%d" format
