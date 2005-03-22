bc41994e0dddc6399935ac57cb1d0104aaa00e20
Bruno Haible <bruno@clisp.org>
2005-03-22 15:43:52 +0000

    Renamed directories.

--- autogen.sh
+++ autogen.sh
@@ -24,20 +24,20 @@ autoconf
 automake
 
 (cd autoconf-lib-link
- aclocal -I m4 -I ../config/m4
+ aclocal -I m4 -I ../m4
  autoconf
  automake
 )
 
 (cd gettext-runtime
- aclocal -I m4 -I ../gettext-tools/m4 -I ../autoconf-lib-link/m4 -I ../config/m4
+ aclocal -I m4 -I ../gettext-tools/m4 -I ../autoconf-lib-link/m4 -I ../m4
  autoconf
  autoheader && touch config.h.in
  automake
 )
 
 (cd gettext-runtime/libasprintf
- aclocal -I ../../config/m4 -I ../m4
+ aclocal -I ../../m4 -I ../m4
  autoconf
  autoheader && touch config.h.in
  automake
@@ -46,10 +46,10 @@ automake
 cp -p gettext-runtime/ABOUT-NLS gettext-tools/ABOUT-NLS
 
 (cd gettext-tools
- aclocal -I m4 -I ../gettext-runtime/m4 -I ../autoconf-lib-link/m4 -I ../config/m4
+ aclocal -I m4 -I ../gettext-runtime/m4 -I ../autoconf-lib-link/m4 -I ../m4
  autoconf
  autoheader && touch config.h.in
  automake
 )
 
-cp -p autoconf-lib-link/config.rpath config/config.rpath
+cp -p autoconf-lib-link/config.rpath build-aux/config.rpath
