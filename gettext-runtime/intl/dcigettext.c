43733a0da83b25554b5a091088abd3006a7cd877
Bruno Haible <bruno@clisp.org>
2005-05-09 19:16:55 +0000

    glibc 2003-06-11  Ulrich Drepper  <drepper@redhat.com> Move the hash_string function to a separate file hash-string.c

--- gettext-runtime/intl/dcigettext.c
+++ gettext-runtime/intl/dcigettext.c
@@ -809,7 +809,7 @@ _nl_find_msg (struct loaded_l10nfile *domain_file,
     {
       /* Use the hashing table.  */
       nls_uint32 len = strlen (msgid);
-      nls_uint32 hash_val = hash_string (msgid);
+      nls_uint32 hash_val = __hash_string (msgid);
       nls_uint32 idx = hash_val % domain->hash_size;
       nls_uint32 incr = 1 + (hash_val % (domain->hash_size - 2));
 
