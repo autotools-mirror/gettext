6997c7c0cde5f9b963d324c9a11e6b68dd0be77b
Bruno Haible <bruno@clisp.org>
2006-07-14 12:13:51 +0000

    Avoid a link error on mingw.

--- gettext-runtime/intl/printf.c
+++ gettext-runtime/intl/printf.c
@@ -1,5 +1,5 @@
 /* Formatted output to strings, using POSIX/XSI format strings with positions.
-   Copyright (C) 2003 Free Software Foundation, Inc.
+   Copyright (C) 2003, 2006 Free Software Foundation, Inc.
    Written by Bruno Haible <bruno@clisp.org>, 2003.
 
    This program is free software; you can redistribute it and/or modify it
@@ -60,6 +60,14 @@ char *alloca ();
 #endif
 
 #define STATIC static
+
+/* This needs to be consistent with libgnuintl.h.in.  */
+#if defined __NetBSD__ || defined __CYGWIN__ || defined __MINGW32__
+/* Don't break __attribute__((format(printf,M,N))).
+   This redefinition is only possible because the libc in NetBSD, Cygwin,
+   mingw does not have a function __printf__.  */
+# define libintl_printf __printf__
+#endif
 
 /* Define auxiliary functions declared in "printf-args.h".  */
 #include "printf-args.c"
