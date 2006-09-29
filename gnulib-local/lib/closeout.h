879b4fa22036c781c038e7605aa5d04e84ced9f8
Bruno Haible <bruno@clisp.org>
2006-09-29 13:46:38 +0000

    Comment change.

--- gnulib-local/lib/closeout.h
+++ gnulib-local/lib/closeout.h
@@ -1,5 +1,5 @@
-/* close standard output
-   Copyright (C) 1998-2003 Free Software Foundation, Inc.
+/* Close standard output and standard error.
+   Copyright (C) 1998-2003, 2006 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
