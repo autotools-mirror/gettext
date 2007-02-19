d76ce0fea98ac80e35f9abb5ccc554940fbc696f
Bruno Haible <bruno@clisp.org>
2007-02-19 03:33:02 +0000

    exit.h is gone.

--- gettext-tools/tests/tstngettext.c
+++ gettext-tools/tests/tstngettext.c
@@ -1,5 +1,5 @@
 /* ngettext - retrieve plural form strings from message catalog and print them.
-   Copyright (C) 1995-1997, 2000-2006 Free Software Foundation, Inc.
+   Copyright (C) 1995-1997, 2000-2007 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -31,7 +31,6 @@
 #include "progname.h"
 #include "relocatable.h"
 #include "basename.h"
-#include "exit.h"
 #include "propername.h"
 #include "xsetenv.h"
 
