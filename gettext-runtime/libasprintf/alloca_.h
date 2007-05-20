eeae57872eb0c51040f1075171e2bd0829105ded
Bruno Haible <bruno@clisp.org>
2007-05-20 20:40:13 +0000

    Update from gnulib.

--- gettext-runtime/libasprintf/alloca_.h
+++ gettext-runtime/libasprintf/alloca_.h
@@ -1,5 +1,5 @@
 /* Memory allocation on the stack.
-   Copyright (C) 1995, 1999, 2001-2005 Free Software Foundation, Inc.
+   Copyright (C) 1995, 1999, 2001-2007 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU Library General Public License as published
@@ -22,8 +22,8 @@
 
 /* Avoid using the symbol _ALLOCA_H here, as Bison assumes _ALLOCA_H
    means there is a real alloca function.  */
-#ifndef _GNULIB_ALLOCA_H
-#define _GNULIB_ALLOCA_H
+#ifndef _GL_ALLOCA_H
+#define _GL_ALLOCA_H
 
 /* alloca(N) returns a pointer (void* or char*) to N bytes of memory
    allocated on the stack, and which will last until the function returns.
@@ -36,35 +36,35 @@
        request, the program just crashes.
  */
 
-#ifdef __GNUC__
-# ifndef alloca
-#  define alloca __builtin_alloca
-# endif
-#else
-# ifdef _MSC_VER
-#  include <malloc.h>
-#  define alloca _alloca
+#ifndef alloca
+# ifdef __GNUC__
+#   define alloca __builtin_alloca
 # else
-#  if HAVE_ALLOCA_H
-#   include <alloca.h>
+#  ifdef _MSC_VER
+#   include <malloc.h>
+#   define alloca _alloca
 #  else
-#   ifdef _AIX
- #pragma alloca
+#   if HAVE_ALLOCA_H
+#    include <alloca.h>
 #   else
-#    ifdef __hpux /* This section must match that of bison generated files. */
-#     ifdef __cplusplus
+#    ifdef _AIX
+ #pragma alloca
+#    else
+#     ifdef __hpux /* This section must match that of bison generated files. */
+#      ifdef __cplusplus
 extern "C" void *alloca (unsigned int);
-#     else /* not __cplusplus */
+#      else /* not __cplusplus */
 extern void *alloca ();
-#     endif /* not __cplusplus */
-#    else /* not __hpux */
-#     ifndef alloca
+#      endif /* not __cplusplus */
+#     else /* not __hpux */
+#      ifndef alloca
 extern char *alloca ();
-#     endif
-#    endif /* __hpux */
+#      endif
+#     endif /* __hpux */
+#    endif
 #   endif
 #  endif
 # endif
 #endif
 
-#endif /* _GNULIB_ALLOCA_H */
+#endif /* _GL_ALLOCA_H */
