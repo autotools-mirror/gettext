e5ef6e6fb2bd3e8ed8c8b9d875dbbfe910e0a888
Bruno Haible <bruno@clisp.org>
2007-06-17 22:10:48 +0000

    Fix typo.

--- gettext-tools/misc/po-mode.el
+++ gettext-tools/misc/po-mode.el
@@ -1547,7 +1547,7 @@ If WRAP is not nil, the search may wrap around the buffer."
   (po-find-span-of-entry)
   (if (or (eq po-entry-type 'untranslated)
 	  (eq po-entry-type 'obsolete)
-	  (y-or-n-p (_"Really loose previous translation? ")))
+	  (y-or-n-p (_"Really lose previous translation? ")))
       (po-set-msgstr (po-get-msgid nil)))
   (message ""))
 
