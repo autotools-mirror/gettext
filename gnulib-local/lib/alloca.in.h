7c4e256ecddc8e937cc724df6dee19a0936bb69c
Bruno Haible <bruno@clisp.org>
2007-10-06 00:56:56 +0000

    Update copyright year.

--- gnulib-local/lib/alloca.in.h
+++ gnulib-local/lib/alloca.in.h
@@ -1,5 +1,5 @@
 /* Memory allocation on the stack.
-   Copyright (C) 1995, 1999, 2001-2005 Free Software Foundation, Inc.
+   Copyright (C) 1995, 1999, 2001-2007 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published
