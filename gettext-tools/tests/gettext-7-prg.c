0042f2e37b7c317a21be69fb6de91a032f2e38f9
Bruno Haible <bruno@clisp.org>
2007-12-26 16:04:23 +0000

    Update after change of 'setenv' module in gnulib.

--- gettext-tools/tests/gettext-7-prg.c
+++ gettext-tools/tests/gettext-7-prg.c
@@ -1,5 +1,5 @@
 /* Test program, used by the gettext-7 test.
-   Copyright (C) 2005-2006 Free Software Foundation, Inc.
+   Copyright (C) 2005-2007 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -28,7 +28,6 @@
 #if USE_POSIX_THREADS && ((__GLIBC__ > 2) || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 3))
 
 #include <pthread.h>
-#include "setenv.h"
 
 /* Make sure we use the included libintl, not the system's one. */
 #undef _LIBINTL_H
