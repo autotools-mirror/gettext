4f6eb1913b4fc62dae19d432016eb35cfb856f19
Bruno Haible <bruno@clisp.org>
2007-12-26 15:46:24 +0000

    Next release will be 0.18.

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=0.17
-RELEASE_DATE=2007-11-07      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.18-pre1
+RELEASE_DATE=2007-12-24      # in "date +%Y-%m-%d" format
