66050275e12d3e168df40696bb3081a301d16a5e
Bruno Haible <bruno@clisp.org>
2010-03-27 21:07:33 +0100

    Update an absolute filename.

--- gettext-runtime/doc/Admin/run.sh
+++ gettext-runtime/doc/Admin/run.sh
@@ -3,7 +3,7 @@ if test -f matrix.xml; then
   mv -f matrix.xml matrix.xml~
 fi
 wget http://translationproject.org/extra/matrix.xml
-JAVA_HOME=/gfs/petix/Volumes/ExtData/bin.x86-linux/inst-java/jdk-1.5.0_06
+JAVA_HOME=/arch/x86-linux/inst-java/jdk-1.5.0_06
 export JAVA_HOME
 PATH="$JAVA_HOME/bin:$PATH"
 java -classpath .:jdom-1.0.jar Matrix
