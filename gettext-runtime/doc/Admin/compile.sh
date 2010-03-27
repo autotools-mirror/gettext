223556594be76589693c90eeb36b1c02895254ce
Bruno Haible <bruno@clisp.org>
2010-03-27 21:09:42 +0100

    Update an absolute filename.

--- gettext-runtime/doc/Admin/compile.sh
+++ gettext-runtime/doc/Admin/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-JAVA_HOME=/gfs/petix/Volumes/ExtData/bin.x86-linux/inst-java/jdk-1.5.0_06
+JAVA_HOME=/arch/x86-linux/inst-java/jdk-1.5.0_06
 export JAVA_HOME
 PATH="$JAVA_HOME/bin:$PATH"
 javac -d . -classpath jdom-1.0.jar Matrix.java
