03731ba189abca0f382eb3cbb0aebfc43dfe1c93
Bruno Haible <bruno@clisp.org>
2018-03-24 16:00:50 +0100

    Comment.
    * .dir-locals.el: Add comment.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,7 @@
+;;; This file contains customizations for Emacs, that apply when editing
+;;; files in this directory and its subdirectories.
+;;; See https://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html
+
+;; Force indentation without tabs.
 ((c-mode . ((indent-tabs-mode . nil)))
  (sh-mode . ((indent-tabs-mode . nil) (sh-basic-offset . 2))))
