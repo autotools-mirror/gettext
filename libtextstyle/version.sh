a4dd134e1afa8102fb72786f9f74e88fb4546ca5
Bruno Haible <bruno@clisp.org>
2019-02-12 23:12:42 +0100

    libtextstyle: Bump version number.
    * libtextstyle/version.sh (VERSION): Set to 0.6.

--- libtextstyle/version.sh
+++ libtextstyle/version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=0.5
-RELEASE_DATE=2019-02-10      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.6
+RELEASE_DATE=2019-02-12      # in "date +%Y-%m-%d" format
