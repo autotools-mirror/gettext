30bd83beb4667d9ed15ee71fb626613ae8372d93
Bruno Haible <bruno@clisp.org>
2020-04-13 15:23:55 +0200

    maint: Update download location of jdom-1.0.jar.

--- gettext-runtime/doc/Admin/compile.sh
+++ gettext-runtime/doc/Admin/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test -f jdom-1.0.jar || wget http://central.maven.org/maven2/jdom/jdom/1.0/jdom-1.0.jar
+test -f jdom-1.0.jar || wget https://repo1.maven.org/maven2/jdom/jdom/1.0/jdom-1.0.jar
 
 JAVA_HOME=/arch/x86-linux/inst-java/jdk-1.5.0_06
 export JAVA_HOME
