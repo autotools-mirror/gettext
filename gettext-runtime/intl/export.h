2902712579032c0010dfc7bc0227877525754254
Bruno Haible <bruno@clisp.org>
2022-07-03 16:21:52 +0200

    build: Fix link error for tstgettext.exe on mingw.
    * gettext-runtime/intl/export.h: Treat mingw like MSVC. In static builds. don't
    use __declspec(dllexport). In shared builds, use also __declspec(dllimport).

--- gettext-runtime/intl/export.h
+++ gettext-runtime/intl/export.h
@@ -1,8 +1,10 @@
 
 #if @HAVE_VISIBILITY@ && BUILDING_LIBINTL
 #define LIBINTL_DLL_EXPORTED __attribute__((__visibility__("default")))
-#elif defined _MSC_VER && BUILDING_LIBINTL
+#elif (defined _WIN32 && !defined __CYGWIN__) && defined WOE32DLL && BUILDING_LIBINTL
 #define LIBINTL_DLL_EXPORTED __declspec(dllexport)
+#elif (defined _WIN32 && !defined __CYGWIN__) && defined WOE32DLL
+#define LIBINTL_DLL_EXPORTED __declspec(dllimport)
 #else
 #define LIBINTL_DLL_EXPORTED
 #endif
