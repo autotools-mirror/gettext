1eaf669a933330ca1985bcd4e0e829df45b827b0
Bruno Haible <bruno@clisp.org>
2022-09-13 21:46:44 +0200

    build: Restore the ability to run "make check".
    * cfg.mk (gl_public_submodule_commit): Define to empty.

--- cfg.mk
+++ cfg.mk
@@ -3,3 +3,7 @@ SED ?= sed
 update-copyright-env = \
   UPDATE_COPYRIGHT_USE_INTERVALS=1 \
   UPDATE_COPYRIGHT_MAX_LINE_LENGTH=79
+
+# Disable a nonsensical maint.mk rule.
+# See <https://lists.gnu.org/archive/html/bug-gnulib/2022-09/msg00074.html>.
+gl_public_submodule_commit =
