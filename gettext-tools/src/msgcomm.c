9464666921935e0430af1a8b2586cffae97218ab
Bruno Haible <bruno@clisp.org>
2022-10-10 01:18:06 +0200

    Merge from 0.21.x branch: Prepare for 0.21.1 release.
    * gettext-runtime/doc/matrix.texi: Update from TP.
    * gettext-runtime/doc/nls.texi (STATUS): Update.
    * gettext-runtime/intl/libgnuintl.in.h (LIBINTL_VERSION): Bump.
    * gettext-tools/libgettextpo/gettext-po.in.h (LIBGETTEXTPO_VERSION): Bump.
    * libtextstyle/version.sh: Update VERSION_NUMBER, RELEASE_DATE.
    * NEWS, gettext-runtime/NEWS: Update.
    * libtextstyle/lib/Makefile.am (LTV_*): Bump to 1:2:1.
    * gettext-runtime/intl/Makefile.am (LTV_*): Bump to 11:0:3.
    * gettext-tools/libgettextpo/Makefile.am (LTV_*): Bump to 5:8:5.
    * gettext-runtime/src/envsubst.c: Update copyright years of the --version output.
    * gettext-runtime/src/gettext.c: Likewise.
    * gettext-runtime/src/gettext.sh.in: Likewise.
    * gettext-runtime/src/ngettext.c: Likewise.
    * gettext-tools/misc/autopoint.in: Likewise.
    * gettext-tools/misc/convert-archive.in: Likewise.
    * gettext-tools/misc/gettextize.in: Likewise.
    * gettext-tools/src/cldr-plurals.c: Likewise.
    * gettext-tools/src/hostname.c: Likewise.
    * gettext-tools/src/msgattrib.c: Likewise.
    * gettext-tools/src/msgcat.c: Likewise.
    * gettext-tools/src/msgcmp.c: Likewise.
    * gettext-tools/src/msgcomm.c: Likewise.
    * gettext-tools/src/msgconv.c: Likewise.
    * gettext-tools/src/msgen.c: Likewise.
    * gettext-tools/src/msgexec.c: Likewise.
    * gettext-tools/src/msgfilter.c: Likewise.
    * gettext-tools/src/msgfmt.c: Likewise.
    * gettext-tools/src/msggrep.c: Likewise.
    * gettext-tools/src/msginit.c: Likewise.
    * gettext-tools/src/msgmerge.c: Likewise.
    * gettext-tools/src/msgunfmt.c: Likewise.
    * gettext-tools/src/msguniq.c: Likewise.
    * gettext-tools/src/recode-sr-latin.c: Likewise.
    * gettext-tools/src/urlget.c: Likewise.
    * gettext-tools/src/xgettext.c: Likewise.
    * gettext-tools/configure.ac (ARCHIVE_VERSION): Bump to 0.21.1.
    * gettext-tools/misc/autopoint.in: Allow version 0.21.1.

--- gettext-tools/src/msgcomm.c
+++ gettext-tools/src/msgcomm.c
@@ -1,5 +1,5 @@
 /* GNU gettext - internationalization aids
-   Copyright (C) 1997-1998, 2000-2007, 2009-2020 Free Software Foundation, Inc.
+   Copyright (C) 1997-1998, 2000-2007, 2009-2022 Free Software Foundation, Inc.
 
    This file was written by Peter Miller <millerp@canb.auug.org.au>
 
@@ -284,7 +284,7 @@ License GPLv3+: GNU GPL version 3 or later <%s>\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n\
 "),
-              "1995-2020", "https://gnu.org/licenses/gpl.html");
+              "1995-2022", "https://gnu.org/licenses/gpl.html");
       printf (_("Written by %s.\n"), proper_name ("Peter Miller"));
       exit (EXIT_SUCCESS);
     }
