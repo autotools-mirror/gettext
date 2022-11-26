21e08dd928f6b1484989a4b2a01df19b27757f4c
Bruno Haible <bruno@clisp.org>
2022-11-26 17:58:32 +0100

    In 'trap' commands, prefer symbolic to numeric signal names.
    * libtextstyle/gnulib-local/tests/test-term-ostream-xterm.sh: Use symbolic
    signal names.

--- libtextstyle/gnulib-local/tests/test-term-ostream-xterm.sh
+++ libtextstyle/gnulib-local/tests/test-term-ostream-xterm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tmpfiles=""
-trap 'rm -fr $tmpfiles' 1 2 3 15
+trap 'rm -fr $tmpfiles' HUP INT QUIT TERM
 
 tmpfiles="$tmpfiles out1 out"
 # The redirection of stderr into a pipe avoids the output of padding bytes
