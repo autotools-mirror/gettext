8752367c1cbd1a5c231e423dbd1c186ea1c67841
Bruno Haible <bruno@clisp.org>
2023-09-06 20:59:03 +0200

    intl: Don't export symbols from static MSVC .obj files.
    * gettext-runtime/intl/configure.ac (WOE32DLL): Set to 1 or 0, not 'yes' or
    'no'. Don't define as C macro any more.
    (DLL_VARIABLE): Test DLL_EXPORT, not _DLL.
    * gettext-runtime/intl/export.h: Use the value of WOE32DLL at configure time.
    Set LIBINTL_DLL_EXPORTED to empty on MSVC when DLL_EXPORT is not defined.
    * gettext-runtime/intl/Makefile.am (libgnuintl.h): Substitute the value of
    @WOE32DLL@.
    * gettext-runtime/intl/intl-compat.c (DLL_EXPORTED): Set to empty on MSVC when
    DLL_EXPORT is not defined.
    * gettext-runtime/intl/printf.c (DLL_EXPORTED): Likewise.
    * gettext-runtime/intl/setlocale.c (DLL_EXPORTED): Likewise.

--- gettext-runtime/intl/export.h
+++ gettext-runtime/intl/export.h
@@ -1,5 +1,5 @@
 /* Control of exported symbols from libintl.
-   Copyright (C) 2005-2022 Free Software Foundation, Inc.
+   Copyright (C) 2005-2023 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
@@ -15,11 +15,20 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #if @HAVE_VISIBILITY@ && BUILDING_LIBINTL
-#define LIBINTL_DLL_EXPORTED __attribute__((__visibility__("default")))
-#elif (defined _WIN32 && !defined __CYGWIN__) && defined WOE32DLL && BUILDING_LIBINTL
-#define LIBINTL_DLL_EXPORTED __declspec(dllexport)
-#elif (defined _WIN32 && !defined __CYGWIN__) && defined WOE32DLL
-#define LIBINTL_DLL_EXPORTED __declspec(dllimport)
+# define LIBINTL_DLL_EXPORTED __attribute__((__visibility__("default")))
+#elif (defined _WIN32 && !defined __CYGWIN__) && @WOE32DLL@ && BUILDING_LIBINTL
+/* The symbols from this file should be exported if and only if the object
+   file gets included in a DLL.  Libtool, on Windows platforms, defines
+   the C macro DLL_EXPORT (together with PIC) when compiling for a DLL
+   and does not define it when compiling an object file meant to be linked
+   statically into some executable.  */
+# if defined DLL_EXPORT
+#  define LIBINTL_DLL_EXPORTED __declspec(dllexport)
+# else
+#  define LIBINTL_DLL_EXPORTED
+# endif
+#elif (defined _WIN32 && !defined __CYGWIN__) && @WOE32DLL@
+# define LIBINTL_DLL_EXPORTED __declspec(dllimport)
 #else
-#define LIBINTL_DLL_EXPORTED
+# define LIBINTL_DLL_EXPORTED
 #endif
