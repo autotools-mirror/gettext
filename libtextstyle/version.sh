2563e4f12c8f2cdc8cf8c7c19954559794c95b18
Bruno Haible <bruno@clisp.org>
2023-10-04 16:53:00 +0200

    Prepare for 0.22.3 release.
    * gettext-runtime/intl/libgnuintl.in.h (LIBINTL_VERSION): Bump.
    * gettext-tools/libgettextpo/gettext-po.in.h (LIBGETTEXTPO_VERSION): Bump.
    * libtextstyle/version.sh: Update VERSION_NUMBER, RELEASE_DATE.
    * gettext-tools/configure.ac (ARCHIVE_VERSION): Bump to 0.22.3.
    * gettext-tools/misc/autopoint.in: Allow version 0.22.3.

--- libtextstyle/version.sh
+++ libtextstyle/version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=0.22.2
-RELEASE_DATE=2023-09-19      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.22.3
+RELEASE_DATE=2023-10-04      # in "date +%Y-%m-%d" format
