23b3c788d40fdcebe0edfe173ee03348fadb798f
Bruno Haible <bruno@clisp.org>
2024-09-29 20:39:32 +0200

    build: Improve customizations.
    * cfg.mk (_autoreconf): Define, to avoid running autoreconf.
    (update-copyright-env): Set UPDATE_COPYRIGHT_MAX_LINE_LENGTH to 999, not 79.

--- cfg.mk
+++ cfg.mk
@@ -1,8 +1,12 @@
-SED ?= sed
+SED = sed
 
+# autoreconf is unsupported in this package! Use autogen.sh instead.
+_autoreconf = ./autogen.sh
+
+# Avoid line breaks in Copyright lines.
 update-copyright-env = \
   UPDATE_COPYRIGHT_USE_INTERVALS=1 \
-  UPDATE_COPYRIGHT_MAX_LINE_LENGTH=79
+  UPDATE_COPYRIGHT_MAX_LINE_LENGTH=999
 
 # Disable a nonsensical maint.mk rule.
 # See <https://lists.gnu.org/archive/html/bug-gnulib/2022-09/msg00074.html>.
