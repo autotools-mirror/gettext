fa4fc7c697ca6dc5b53aa75022753406cb485c11
Bruno Haible <bruno@clisp.org>
2024-12-16 13:49:38 +0100

    hello-c-gnome3: Exit when the window's close button is clicked.
    * gettext-tools/examples/hello-c-gnome3/hello.c
    (hello_application_window_dispose): Remove function.
    (hello_application_window_class_init): Don't overwrite the 'dispose' field.

--- gettext-tools/examples/hello-c-gnome3/hello.c
+++ gettext-tools/examples/hello-c-gnome3/hello.c
@@ -94,18 +94,9 @@ hello_application_window_init (HelloApplicationWindow *window)
 }
 
 static void
-hello_application_window_dispose (GObject *object)
-{
-  HelloApplicationWindow *window = HELLO_APPLICATION_WINDOW (object);
-  g_clear_object (&window->settings);
-}
-
-static void
 hello_application_window_class_init (HelloApplicationWindowClass *klass)
 {
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
-
-  gobject_class->dispose = hello_application_window_dispose;
 
   gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
                                                UI_PATH);
