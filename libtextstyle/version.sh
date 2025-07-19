3ed149a373493516108d80703cd314badaae501e
Bruno Haible <bruno@clisp.org>
2025-07-19 17:08:04 +0200

    Prepare for 0.26 release.
    * gettext-runtime/doc/matrix.texi: Update from TP.
    * gettext-runtime/doc/nls.texi (STATUS): Update.
    * gettext-runtime/intl/libgnuintl.in.h (LIBINTL_VERSION): Bump.
    * libtextstyle/version.sh: Update VERSION_NUMBER, RELEASE_DATE.
    * gettext-tools/libgettextpo/gettext-po.in.h (LIBGETTEXTPO_VERSION): Bump.
    * gettext-runtime/po/Makefile.in.in: Update Origin.
    * gettext-runtime/intl/Makefile.am (LTV_*): Bump to 12:5:4.
    * libtextstyle/lib/Makefile.am (LTV_*): Bump to 2:6:2.
    * gettext-tools/libgettextpo/Makefile.am (LTV_*): Bump to 5:15:5.
    * gettext-tools/misc/autopoint.in: Likewise. Accept version 0.26.
    * gettext-tools/configure.ac (ARCHIVE_VERSION): Bump to 0.26.

--- libtextstyle/version.sh
+++ libtextstyle/version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=0.25
-RELEASE_DATE=2025-05-07      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.26
+RELEASE_DATE=2025-07-19      # in "date +%Y-%m-%d" format
