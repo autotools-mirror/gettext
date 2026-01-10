144474ff645593d9476120dd6720d8c0fe1bd5d3
Bruno Haible <bruno@clisp.org>
2026-01-10 22:17:40 +0100

    libtextstyle: Assume a working <stdbool.h>.
    * libtextstyle/lib/textstyle.h: Include <stdbool.h> instead of
    <textstyle/stdbool.h>.
    * libtextstyle/lib/Makefile.am (nobase_nodist_include_HEADERS): Don't add
    textstyle/stdbool.h.
    (textstyle/stdbool.h): Remove rule.
    (BUILT_SOURCES, MOSTLYCLEANFILES, CLEANFILES): Update accordingly.
    (EXTRA_DIST): Remove stdbool.mini.h.
    * libtextstyle/lib/stdbool.mini.h: Remove file.
    * gettext-tools/src/Makefile.am (textstyle/stdbool.h): Remove target.
    (BUILT_SOURCES, MOSTLYCLEANFILES): Remove textstyle/stdbool.h.

--- libtextstyle/lib/textstyle.h
+++ libtextstyle/lib/textstyle.h
@@ -1,5 +1,5 @@
 /* Public API of the libtextstyle library.
-   Copyright (C) 2006-2007, 2019-2021 Free Software Foundation, Inc.
+   Copyright (C) 2006-2007, 2019-2026 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -20,9 +20,9 @@
 #define _TEXTSTYLE_H
 
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <textstyle/stdbool.h>
 #include <textstyle/woe32dll.h>
 
 /* Meta information.  */
